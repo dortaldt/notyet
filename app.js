6a822094d8a89ccba852664db3202f5dd5200ab0
dortaldt <dortaldt@gmail.com>
2017-10-02 23:09:09 +0200

    new style fix

--- app.js
+++ app.js
@@ -14,14 +14,18 @@ app.get('/', function (req, res) {
 	var pg_des = req.query.pg_des
 	var pg_url = req.originalUrl
 
-	//in case no question is given, load home page:
-	if(typeof que == 'undefined') {
-		res.send(pug.renderFile( __dirname + '/views' + '/home_page.pug',
+	var landingPage = pug.renderFile( __dirname + '/views' + '/home_page.pug',
 			{
 			page_title: "Welcome to Not Yet",
-			page_img: "",				
+			page_img: pg_img,
+  			page_des: pg_des,
+  			page_url: pg_url				
 			}
-		));
+		)
+
+	//in case no question is given, load home page:
+	if(typeof que == 'undefined') {
+		res.send(landingPage);
 	}
 
 	//send variables to temlate file and render result:
@@ -29,6 +33,7 @@ app.get('/', function (req, res) {
 	  	res.send(pug.renderFile( __dirname + '/views' + '/notyet_page.pug',
 	  		{
   			question: que,
+  			answer: ans,
   			page_title: pg_title, 
   			page_img: pg_img,
   			page_des: pg_des,
