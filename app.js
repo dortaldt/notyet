8e035e1c87583c4380019ec7a87d086e000fd696
dortaldt <dortaldt@gmail.com>
2017-10-05 12:24:32 +0200

    trying to deploy to Heroku

--- app.js
+++ app.js
@@ -79,3 +79,7 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
+});
