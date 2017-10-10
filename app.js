ae2251f8c822cee35e57e70d6ba35f4bfdfef330
dortaldt <dortaldt@gmail.com>
2017-10-10 17:48:14 +0200

    fix header landing page

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ app.get('/', function (req, res) {
 			{
 				page_title: "Welcome to Not Yet",
 				page_img: 'https://notyetapp.herokuapp.com' + '/assets/and_now.png',
-	  			page_des: "Are we there yet? No! Give others link and stop getting the same answer",
+	  			page_des: "Are we there yet? No! Give others link and stop getting the same question",
 	  			page_url: pg_url,
 	  			background_image: bg_img,
 	  			logo: logo,
