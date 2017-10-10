ceaedc896754f1284fa45b007adab10cce94b1ed
dortaldt <dortaldt@gmail.com>
2017-10-10 17:31:44 +0200

    upadte img and desc for landing

--- app.js
+++ app.js
@@ -192,8 +192,8 @@ app.get('/', function (req, res) {
 		var landingPage = pug.renderFile( __dirname + '/views' + '/home_page.pug',
 			{
 				page_title: "Welcome to Not Yet",
-				page_img: pg_img,
-	  			page_des: pg_des,
+				page_img: '../assets/and_now.png',
+	  			page_des: "Are we there yet? No! Give others link and stop getting the same answer",
 	  			page_url: pg_url,
 	  			background_image: bg_img,
 	  			logo: logo,
